ecd902f700f496eed73bea2ace541d928b4cd49e
Alex Jang <alexjang1993@gmail.com>
2024-01-25 19:07:09 -0800

    Initial Commit

---
+++ SportsGrid/SportsGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsGridApp.swift
+//  SportsGrid
+//
+//  Created by Alex Jang on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SportsGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
